7777f5340069adedba613a4440788e3e1f9d8ae2
COOLIRON2311 <ignatenko.ivan.23112000@gmail.com>
2021-01-28 16:47:11 +0300

    Added main

--- pysrc/confparser.py
+++ pysrc/confparser.py
@@ -5,3 +5,8 @@ from config import *
 
 args = f'-msize {MAX_FILE_SIZE} -threat {REMOVE_THREATS} -t {" ".join(SCAN_TARGETS)} -e {" ".join(SCAN_EXCLUDE)} -q {QUARANTINE_PATH}'
 
+def main() -> None:
+    print(args)
+
+if __name__ == '__main__':
+    main()
