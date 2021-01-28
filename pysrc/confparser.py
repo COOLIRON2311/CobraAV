d51f3a90d44115637be76be171234ecd94a61313
COOLIRON2311 <ignatenko.ivan.23112000@gmail.com>
2021-01-28 16:40:32 +0300

    Added shebang

--- pysrc/confparser.py
+++ pysrc/confparser.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 from config import *
 
 args = f'-msize {MAX_FILE_SIZE} -threat {REMOVE_THREATS} -t {" ".join(SCAN_TARGETS)} -e {" ".join(SCAN_EXCLUDE)} -q {QUARANTINE_PATH}'
