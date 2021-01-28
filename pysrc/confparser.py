c48ae4aa706d0c91bf2909c91f1c1964ad431ef7
Urdeney <andrewnureev142@gmail.com>
2021-01-28 19:22:07 +0300

    Added docs

--- pysrc/confparser.py
+++ pysrc/confparser.py
@@ -6,6 +6,18 @@ from config import *
 args = f'-msize {MAX_FILE_SIZE} -threat {REMOVE_THREATS} -t {" ".join(SCAN_TARGETS)} -e {" ".join(SCAN_EXCLUDE)} -q {QUARANTINE_PATH}'
 
 def main() -> None:
+    """
+Parser arguments docs:
+----------------------
+
+-msize - Max files size to scan (in bytes). Files of greater size will be ignored.
+-threat - Action on threat detection:
+          0 - Move threat to quarantine
+          1 - Remove infected file
+-t - Scan target directories recursively
+-e - Exclude files and directories from scan query by wildcard
+-q - Quarantine directory path
+"""
     print(args)
 
 if __name__ == '__main__':
