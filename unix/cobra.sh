65e20711689d0c983a5a5d5b7271d65e14e605a6
COOLIRON2311 <ignatenko.ivan.23112000@gmail.com>
2021-02-07 20:53:12 +0300

    Updated bash completion

--- unix/cobra.sh
+++ unix/cobra.sh
@@ -1,2 +1,26 @@
 #/usr/bin/env bash
-complete -W "start stop status config" cobra
+
+_cobra ()
+{
+    local cur=${COMP_WORDS[COMP_CWORD]}
+    case $COMP_CWORD in
+    1)
+        COMPREPLY=( $(compgen -W "scan start stop status config whitelist blacklist contain decontain remove list-threats clear-threats" -- $cur) )
+        return 0;;
+    2)
+        local IFS=$'\n'
+        case "${COMP_WORDS[1]}" in
+            scan|whitelist|blacklist|contain)
+                COMPREPLY=( $(compgen -o default -- $cur) );
+                return 0;;
+            remove|decontain)
+                COMPREPLY=( $(compgen -W "$(find /opt/cobraav/quarantine/!(*.avmeta) -printf '%f\n' 2>/dev/null)" -- $cur) )
+                return 0;;
+            *)
+                return 0;;
+        esac
+        ;;
+    esac
+}
+
+complete -o filenames -F _cobra cobra
